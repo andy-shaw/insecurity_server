70e98e6c6a55480a62d37813433919f658cd175d
LunasAbacus <lunasabacus@gmail.com>
2013-10-27 17:46:08 -0400

    Small update to harness, clarifying some things with comments

--- InsecureServer/source/Jacobs/Harness/Harness.java
+++ InsecureServer/source/Jacobs/Harness/Harness.java
@@ -8,16 +8,17 @@ public class Harness {
 	
 	public Harness()
 	{
-		// TODO - Nate - Create dataminer
+		// Create dataminer
 		// This keeps hold of all the points as well as additional info
-		miner = new DataMiner(7, 1.0/69.0);
+		miner = new DataMiner(7, 1.0/69.0); // 7 points minimum for cluster
+											// 1 mile max range between points in cluster
 				
-		// 
-		
+		// TODO - Instantiate whatever else here
 	}
 	
 	public void AddPoint(double Longitude, double Latitude)
 	{
+		// Get raw value expressed as Degrees. Convert under assumption 1 degree ~= 60 miles.
 		double raw = miner.AddLocation(Latitude, Longitude);
 		
 		// TODO - raw conversion to error level
